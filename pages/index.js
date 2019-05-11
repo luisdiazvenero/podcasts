2d291526a124dc660b769d71df45205390e88669
Luis Díaz Venero <luisdiazvenero@gmail.com>
2019-05-10 23:04:50 -0500

    vista de inicio, canal y detalle de cada audio

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,71 @@
 import 'isomorphic-fetch'
+import Link from 'next/link'
+
 export default class extends React.Component {
 
   static async getInitialProps() {
     let req = await fetch('https://api.audioboom.com/channels/recommended')
     let { body: channels } = await req.json()
-
     return { channels }
   }
-  render(){
+
+  render() {
     const { channels } = this.props
 
     return <div>
       <header>Podcasts</header>
+
       <div className="channels">
-      { channels.map((channel) => (
-        <div className="channel">
-          <img src={ channel.urls.logo_image.original } alt=""/>
-          <h2>{channel.title}</h2>
-        </div>
-        ))}
+        { channels.map((channel) => (
+          <Link href={`/channel?id=${channel.id}`} prefetch key={ channel.id }>
+            <a className="channel">
+              <img src={ channel.urls.logo_image.original } alt=""/>
+              <h2>{ channel.title }</h2>
+            </a>
+          </Link>
+        )) }
       </div>
-      
-    
-    <style jsx>{`
-      header {
-        color: #fff;
-        background: #8756ca;
-        padding: 15px;
-        text-align: center;
-    }
 
-    .channels {
-      display: grid;
-      grid-gap: 15px;
-      padding: 15px;
-      grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-  }
+      <style jsx>{`
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+        a.channel {
+          display: block;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+        }
+      `}</style>
 
-    .channel {
-      display: block;
-      border-radius: 3px;
-      margin-bottom: 0.5em;
-      box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
-  }
-  .channel img {
-      width: 100%;   
-  }
-  h2 {
-      padding: 5px;
-      font-size: 0.9em;
-      font-weight: 600;
-      maring: 0;
-      text-align: center;
-  }
-    `}</style>
-
-    <style jsx global>
-    {`
-    body {
-      margin: 0;
-      font-family: system-ui;
-      background: white;
-    }
-    `}
-    </style>
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+      `}</style>
     </div>
   }
 }
