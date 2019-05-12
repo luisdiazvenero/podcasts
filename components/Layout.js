6048bba05fa8ddeb528a25524387217c5242f8bb
Luis Díaz Venero <luisdiazvenero@gmail.com>
2019-05-11 23:28:47 -0500

    titulo de pagina

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Head from 'next/head'
 export default class Layout extends React.Component{
     render(){
-        const {children} = this.props
+        const {children, title} = this.props
         return <div>
+            <Head>
+                <title>{title}</title>
+            </Head>
             <header><Link href="/"><a>Podcasts</a></Link> </header>
             {children}
 
